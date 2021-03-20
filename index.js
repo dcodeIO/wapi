8c11242a3b19152345b10b2b8fb0649a66346698
dcode <dcode@dcode.io>
2021-03-20 05:56:22 +0100

    Save two bytes ;)

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export default function wapi(e){const f={};return{env:e,call:(c,...a)=>(f[c]||(f[c]=Function(...Object.keys(e).concat(`return ${e.getString(c)}`)).apply(e,Object.values(e))))(...a)}}
+export default function wapi(e){const f={};return{env:e,call:(c,...a)=>(f[c]||(f[c]=Function(...Object.keys(e).concat("return "+e.getString(c))).apply(e,Object.values(e))))(...a)}}
